b94c0ca92829b69c55df9bc2651bd6b56a4403e1
ShubhankarX86 <90346899+ShubhankarX86@users.noreply.github.com>
2024-06-26 13:53:16 +0530

    Add files via upload

---
+++ DT.cs
@@ -0,0 +1,72 @@
+﻿using multithreading;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace datentime
+{
+    public enum Months
+    {
+        January = 1,
+        Feburary,
+        March,
+        April,
+        May,
+        June,
+        July,
+        August,
+        September,
+        October,
+        November,
+        December
+    }
+    public partial class DT
+    {
+        public static void display(string pat)
+        {
+            DateTime curr = DateTime.Now;
+
+            Console.WriteLine();
+            Console.WriteLine("Upcoming Birthdays: ");
+            Console.WriteLine();
+
+            foreach (string line in File.ReadLines(generateDir2(pat)))
+            {
+                string[] parts = line.Split(',');
+                string name = parts[0];
+                DateTime birthday = DateTime.Parse(parts[1]);
+
+                
+                DateTime thisYearBirthday = new DateTime(curr.Year, birthday.Month, birthday.Day);
+                DateTime nextYearBirthday = thisYearBirthday.AddYears(1);
+
+                
+                int diff = (thisYearBirthday - curr).Days;
+                if (diff < 0)
+                {
+                    diff = (nextYearBirthday - curr).Days;
+                }
+
+                if (diff <= 10 && diff >= 0)
+                {
+                    Console.WriteLine($"{name}: {birthday.Day} {(Months)birthday.Month} ({diff} days from now)");
+                }
+            }
+            Console.WriteLine();
+        }
+
+        public static string generateDir2(string pat)
+        {
+            string path = Assembly.GetExecutingAssembly().Location;
+            DirectoryInfo Dinfo = new DirectoryInfo(path).Parent.Parent.Parent;
+            string myloc = Path.Combine(Dinfo.FullName, pat);
+
+            return myloc;
+        }
+    }
+}
+  
---
+++ Program.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.IO;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using datentime;
+using Microsoft.Win32;
+
+namespace multithreading
+{
+    internal class Program
+    {      
+        static void Main(string[] args)
+        {
+            int loginCounter = 0;
+            int registerCounter = 0;
+            bool state = true;
+
+            while (state)
+            {
+               
+                if (loginCounter == 0)
+                {
+                    Console.WriteLine("Press 1 to login \nPress 2 to create new \nPress 3 to Exit");
+                }
+                else
+                {
+                    Console.WriteLine("Press 1 to login again \nPress 2 to create new \nPress 3 to Exit");
+                }
+
+                char option = char.Parse(Console.ReadLine());
+
+                switch (option)
+                {
+                    case '1':
+                        login();
+                        loginCounter++;
+                        break;
+                    case '2':
+                        Thread registerThread = new Thread(register);
+                        registerThread.Start();
+                        registerThread.Join();
+
+                        Thread loginThread = new Thread(login);
+                        loginThread.Start();
+                        loginThread.Join();
+                        break;
+                    case '3':
+                        state = false;
+                        Console.WriteLine("BYE BYE!! :)");
+                        break;
+
+                    default:
+                        Console.WriteLine("Kindly enter one of the above options: ");
+                        break;
+                }
+            }
+        }
+
+        public static void login()
+        {
+            Dictionary<String, String> userMap = new Dictionary<String, String>();
+            string ID = " ";
+
+            foreach (string line in File.ReadLines(generateDir()))
+            { 
+                userMap.Add(line.Split(',')[0], line.Split(',')[1]);
+                ID = line.Split(',')[0];
+            }
+
+            if(userMap.Count == 0)
+            {
+                Console.WriteLine();
+                Console.WriteLine("DataBase empty!! User not found!!");
+                Console.WriteLine();
+            }
+            else
+            {
+                Console.WriteLine();
+                Console.WriteLine("Welcome " + ID);
+                Console.WriteLine();
+                Console.WriteLine("Employee DB directory: ");
+                try
+                {
+                    string path = Console.ReadLine(); //file format: employeeName,datetime
+                    DT.display(path);
+                }
+                catch (FileNotFoundException ex)
+                {
+                    Console.WriteLine("FileNotFound! {0}", ex.Message);
+                    Console.WriteLine();
+                }
+            }
+        }
+        public static void register()
+        {
+            string str = "";
+
+            foreach (string line in File.ReadLines(generateDir()))
+            {
+                str = line.Split(',')[0];
+            }
+            char registerCounter;
+
+            if (str.Length == 0)
+            {
+                registerCounter = '0';
+            }
+            else
+            {
+                registerCounter = (char)(str[12]+1);
+            }
+
+            string ID = "Shubhankar00" + registerCounter + "@gmail.com";
+            string password = registerCounter + "1" + registerCounter + "6";
+            Program.writeLog(ID + "," + password);
+
+            Console.WriteLine();
+            Console.WriteLine("A guest ID has been created, you're now Logged-in!");
+        }
+
+        public static string generateDir()
+        {
+            string path = Assembly.GetExecutingAssembly().Location;
+            DirectoryInfo Dinfo = new DirectoryInfo(path).Parent.Parent.Parent;
+            string finPath = Path.Combine(Dinfo.FullName, "userDB.txt");
+
+            return finPath;
+        }
+
+        public static void writeLog(string message)
+        {
+            FileStream fs = new FileStream(Program.generateDir(), FileMode.Open, FileAccess.Write);
+
+           
+            byte[] bytes = Encoding.UTF8.GetBytes(message);
+            fs.Write(bytes, 0, message.Length);
+            fs.Close();
+        }
+    }
+}
